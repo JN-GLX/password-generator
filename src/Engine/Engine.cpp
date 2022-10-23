fa6b9d80a78cf78d1a7b01f03758b2f170ebcfb2
JN GLX <jn.glx@outlook.fr>
2022-10-23 18:23:13 +0200

    Classe Engine: refactorisation

--- src/Engine/Engine.cpp
+++ src/Engine/Engine.cpp
@@ -1,50 +1,82 @@
 #include "Engine.h"
 #include "helpers.h"
 
-Engine::Engine() {
+Engine::Engine() 
+{
     randomEngine.seed(std::chrono::system_clock::now().time_since_epoch().count());
 }
 
-Engine::~Engine() {
+Engine::~Engine() 
+{
 
 }
 
-std::string Engine::generatePassword(int length) {
+std::string Engine::generatePassword(int passwordLength) 
+{
     std::string password = "";
 
-    std::string sourceString = CONSONANTS;
-    sourceString += getUpperString(sourceString);
-    
-    std::uniform_int_distribution<int> nbConsonantsDistribution(1, length-2);
-    int nbConsonants = nbConsonantsDistribution(randomEngine);
-    for(int count = 0; count < nbConsonants; count++)
-    {
-        password += getRandomCharFromString(sourceString);
-    }
+    int nbConsonants = getRandomNumber(passwordLength-2);
+    password = getRandomConsonants(nbConsonants);
 
-    std::uniform_int_distribution<int> numberDistribution(1, length-nbConsonants);
-    int nbVowels = numberDistribution(randomEngine);
-    sourceString = VOWELS;
-    sourceString += getUpperString(sourceString);
+    int nbVowels = getRandomNumber(passwordLength - nbConsonants - 1);
+    password += getRandomVowels(nbVowels);
 
-    for(int count =0; count < nbVowels; count++)
-    {
-        password += getRandomCharFromString(sourceString);
-    }
-
-    for(int count=0; count < length - (nbConsonants + nbVowels); count++)
-    {
-        password += getRandomCharFromString(DIGITS);
-    }
-    
+    int nbDigits = passwordLength - (nbConsonants + nbVowels);
+    password += getRandomDigits(nbDigits);
 
     return password;
 }
 
-std::string Engine::getRandomCharFromString(std::string sourceString) {
+std::string Engine::getRandomConsonants(int nbConsonants) 
+{
+    std::string sourceString = CONSONANTS;
+    sourceString += getUpperString(sourceString);
+
+    std::string randomConsonants = getRandomCharSequence(sourceString, nbConsonants);
+
+    return randomConsonants;
+}
+
+std::string Engine::getRandomVowels(int nbVowels) 
+{
+    std::string sourceString = VOWELS;
+    sourceString += getUpperString(sourceString);
+
+    std::string randomVowels = getRandomCharSequence(sourceString, nbVowels);
+
+    return randomVowels;
+}
+
+std::string Engine::getRandomDigits(int nbDigits) 
+{
+    std::string randomDigits = getRandomCharSequence(DIGITS, nbDigits);
+    return randomDigits;
+}
+
+std::string Engine::getRandomCharFromString(std::string sourceString) 
+{
     std::uniform_int_distribution<int> charDistribution(1, sourceString.length()-1);
     std::string randomChar;
+    
     randomChar = sourceString[charDistribution(randomEngine)];
 
     return randomChar;
+}
+
+int Engine::getRandomNumber(int range) 
+{
+    std::uniform_int_distribution<int> numberDistribution(1, range);
+    return numberDistribution(randomEngine);
+}
+
+std::string Engine::getRandomCharSequence(std::string sourceString, int nbChars) 
+{
+    std::string charSequence = "";
+    
+    for(int count = 0; count < nbChars; count++)
+    {
+        charSequence += getRandomCharFromString(sourceString);
+    }
+
+    return charSequence;
 }
--- src/Engine/Engine.h
+++ src/Engine/Engine.h
@@ -18,8 +18,15 @@ public:
     std::string generatePassword(int length);
 
 private:
+
+    std::string getRandomConsonants(int nbConsonants);
+    std::string getRandomVowels(int nbVowels);
+    std::string getRandomDigits(int nbDigits);
+
+    std::string getRandomCharSequence(std::string sourceString, int nbChars);
     std::string getRandomCharFromString(std::string sourceString);
-    
+    int getRandomNumber(int range);
+
     std::default_random_engine randomEngine;
     static constexpr const char* CONSONANTS = "bcdfghjklmnpqrstvwzx";
     static constexpr const char* VOWELS = "aeiouy";
